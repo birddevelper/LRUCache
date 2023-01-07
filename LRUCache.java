edd3af21bb9559a8434eebff5f727f32a91e5af2
birddevelper <mostafa_shaeri_tj@yahoo.com>
2023-01-06 22:30:38 -0800

    extracted moveForward and evict functions

--- LRUCache.java
+++ LRUCache.java
@@ -32,6 +32,58 @@ class LRUCache {
         storage = new HashMap<Integer, CacheCell>(capacity);
     }
 
+
+    
+
+
+    private void moveToFront(Integer key){
+
+        //if it's already front
+        if (frontElementKey == key){
+            return;
+        }
+
+        // if it's the first and only inserted node, just set the front and tail
+        if(frontElementKey == null && tailElementKey == null){
+            frontElementKey = key;
+            tailElementKey = key;
+        }
+        else {
+            // if it's new node, not already existed, should be put at front
+            if (storage.get(key).next == null && storage.get(key).previous == null){
+                storage.get(key).previous = frontElementKey;
+                storage.get(frontElementKey).next = key;
+                frontElementKey = key;
+                return;
+            }
+            // if it's tail
+            else if (tailElementKey==key){
+                tailElementKey = storage.get(key).next;
+                storage.get(tailElementKey).previous= null;
+                storage.get(frontElementKey).next = key;
+                frontElementKey = key;
+                return;
+            }
+            //if it's middle
+            else if (storage.get(key).next != null && storage.get(key).previous != null){
+                storage.get(storage.get(key).previous).next = storage.get(key).next;
+                storage.get(storage.get(key).next).previous = storage.get(key).previous;
+                storage.get(key).next = null;
+                storage.get(key).previous = frontElementKey;
+                storage.get(frontElementKey).next = key;
+                frontElementKey = key;
+                
+                return;
+            }
+        }
+
+            
+    }
+
+
+
+
+
     // get value of a key
     public int get(Integer key) {
 
@@ -41,42 +93,45 @@ class LRUCache {
         }
 
         CacheCell cacheCell = storage.get(key);
-        // if the cell is tail node, and it's not the only node in storage, change the tail to its next node
-        if(tailElementKey == key && cacheCell.next != null )
-            tailElementKey = cacheCell.next;
 
-        // put the cell in front of linked list as first node
-        cacheCell.previous = frontElementKey;
-        cacheCell.next = null;
-        storage.get(frontElementKey).next = key;
-        frontElementKey = key;
+        moveToFront(key);
 
         return cacheCell.value;
     }
 
     // put a key-value to storage
-    public void put(int key, int value) {
+    public void put(Integer key, Integer value) {
     
         // if the capacity is full and the key doesn't exist, remove the LRU cell (tail node )
         if(storage.size()==this.capacity && !storage.containsKey(key)){
-            Integer tempTailElement = tailElementKey;
-            tailElementKey = (storage.get(tempTailElement).next == null ? key : storage.get(tempTailElement).next);
-            storage.remove(tempTailElement); 
+            evictCell();
         }
-        
-        
-        if(storage.size()!=0)
-            storage.get(frontElementKey).next=key; // if storage was not empty, so we have already a front element, it should refrence to new element
-        else
-            tailElementKey = key; // if the storge is empty, so the only cell is tail element too
 
-        CacheCell cacheCell = new CacheCell(value);
-        cacheCell.previous = frontElementKey;
-        cacheCell.next = null;
-        frontElementKey = key;
-        storage.put(key,cacheCell);
-        storage.get(tailElementKey).previous = null;
+        if(storage.containsKey(key)){
+            storage.get(key).value = value;
+        }
+        else{
+            CacheCell cacheCell = new CacheCell(value);
+            storage.put(key,cacheCell);
+        }
+        moveToFront(key);
 
+    }
+
+
+
+    private void evictCell() {
+        if(storage.size()==1){
+            storage.remove(tailElementKey);
+            frontElementKey = null;
+            tailElementKey = null;
+            return;
+        }
+
+        Integer tempTailElement = tailElementKey;
+        storage.get(storage.get(tailElementKey).next).previous = null;
+        tailElementKey = storage.get(tempTailElement).next;
+        storage.remove(tempTailElement);
     }
     
 }
@@ -128,7 +183,46 @@ class Main {
     System.out.println(lRUCacheWithThreeCap.get(3)); // return 3
     lRUCacheWithThreeCap.put(4, 4);
     System.out.println(lRUCacheWithThreeCap.get(1)); // return -1
-    
+
+    System.out.println("===============");
+    LRUCache lRUCacheWithTwoCap = new LRUCache(2);
+    lRUCacheWithTwoCap.put(2, 1);
+    lRUCacheWithTwoCap.put(1, 1);
+    lRUCacheWithTwoCap.put(2, 3);
+    lRUCacheWithTwoCap.put(4, 1);
+    System.out.println(lRUCacheWithTwoCap.get(1));
+    System.out.println(lRUCacheWithTwoCap.get(2)); 
+
+    // [[2],[2,1],[3,2],[3],[2],[4,3],[2],[3],[4]]
+    System.out.println("===============");
+    lRUCacheWithTwoCap = new LRUCache(2);
+    lRUCacheWithTwoCap.put(2, 1);
+    lRUCacheWithTwoCap.put(3, 2);
+    System.out.println(lRUCacheWithTwoCap.get(3)); // return 3
+    System.out.println(lRUCacheWithTwoCap.get(2)); // return 1
+    lRUCacheWithTwoCap.put(4, 3);
+    System.out.println(lRUCacheWithTwoCap.get(2)); // return 1
+    System.out.println(lRUCacheWithTwoCap.get(3)); // return -1
+    System.out.println(lRUCacheWithTwoCap.get(4)); // return 3
+    System.out.println(lRUCacheWithTwoCap.get(2)); // return 1
+
+
+    //[[3],[1,1],[2,2],[3,3],[4,4],[4],[3],[2],[1],[5,5],[1],[2],[3],[4],[5]]
+    lRUCacheWithTwoCap = new LRUCache(3);
+    lRUCacheWithTwoCap.put(1, 1);
+    lRUCacheWithTwoCap.put(2, 2);
+    lRUCacheWithTwoCap.put(3, 3);
+    lRUCacheWithTwoCap.put(4, 4);
+    System.out.println(lRUCacheWithTwoCap.get(4)); // return 4
+    System.out.println(lRUCacheWithTwoCap.get(3)); // return 3
+    System.out.println(lRUCacheWithTwoCap.get(2)); // return 2
+    System.out.println(lRUCacheWithTwoCap.get(1)); // return -1
+    lRUCacheWithTwoCap.put(5, 5);
+    System.out.println(lRUCacheWithTwoCap.get(1)); // return -1
+    System.out.println(lRUCacheWithTwoCap.get(2)); // return -1
+    System.out.println(lRUCacheWithTwoCap.get(3)); // return 3
+    System.out.println(lRUCacheWithTwoCap.get(4)); // return 4
+    System.out.println(lRUCacheWithTwoCap.get(5)); // return 5
 
   }
 
